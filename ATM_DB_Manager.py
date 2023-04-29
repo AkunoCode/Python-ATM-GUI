eaaac355625e5c3d3eb3d00b11c2af174fd4e9cc
John Leo Echevaria <echevariajohnleo@gmail.com>
2023-04-29 22:05:13 +0800

    Created ATM_Database.sql and ATM_DB_Manager.py

---
+++ ATM_DB_Manager.py
@@ -0,0 +1,123 @@
+import mysql.connector
+from mysql.connector import Error
+from DATABASE_CONFIG import *
+
+class ATM_Manager():
+
+    def __init__(self) -> None:
+        """Initialize the database connection"""
+        self.db = mysql.connector.connect(
+            host=dbhost,
+            user=dbuser,
+            password=dbpass,
+            database=dbname
+        )
+        self.cursor = self.db.cursor()
+
+    def register_users(self, userID, surname, fstname, homeadd, phonenum):
+        """Register a new user to the database"""
+        try:
+            query = "INSERT INTO users (userID, surname, fstname, homeadd, phonenum) VALUES (%s, %s, %s, %s, %s)"
+            vars = (userID, surname, fstname, homeadd, phonenum)
+            self.cursor.execute(query, vars)
+            self.db.commit()
+        except Error as e:
+            print(f"Failed to insert record into MySQL table {e}")
+
+    def register_account(self, account_no, acctype, userID, balance, withdraw_limit, withdraw_freq):
+        """Register a new account to the database"""
+        try:
+            query = "INSERT INTO accounts (account_no, acctype, userID, balance, withdraw_limit, withdraw_freq) VALUES (%s, %s, %s, %s, %s, %s)"
+            vars = (account_no, acctype, userID, balance, withdraw_limit, withdraw_freq)
+            self.cursor.execute(query, vars)
+            self.db.commit()
+        except Error as e:
+            print(f"Failed to insert record into MySQL table {e}")
+
+    def view_one(self, table, condition, value):
+        """View a record from a table"""
+        try:
+            query = f"SELECT * FROM {table} WHERE {condition} = {value}"
+            self.cursor.execute(query)
+            result = self.cursor.fetchone()
+            return result
+        except Error as e:
+            print(f"Failed to select record from MySQL table {e}")
+
+    def view_user(self, userID):
+        """View a user's details by calling view_one()"""
+        return self.view_one("users", "userID", userID)
+        
+    def view_account(self, account_no):
+        """View an account's details by calling view_one()"""
+        return self.view_one("accounts", "account_no", account_no)
+    
+    def view_balance(self, account_no):
+        """View an account's balance"""
+        return self.view_one("accounts", "account_no", account_no)[3]
+    
+    def deposit(self, account_no, amount):
+        """Deposit money into an account"""
+        try:
+            query = f"UPDATE accounts SET balance = balance + {amount} WHERE account_no = {account_no}"
+            self.cursor.execute(query)
+            self.db.commit()
+        except Error as e:
+            print(f"Failed to update record in MySQL table {e}")
+    
+    def view_all_transactions(self, account_no):
+        """View all transactions of an account"""
+        try:
+            query = f"SELECT * FROM transactions WHERE account_no = {account_no}"
+            self.cursor.execute(query)
+            result = self.cursor.fetchall()
+            return result
+        except Error as e:
+            print(f"Failed to select record from MySQL table {e}")
+    
+    def view_day_transactions(self, account_no):
+        """View all transactions of an account within the day"""
+        try:
+            query = f"SELECT * FROM transactions WHERE account_no = {account_no} AND date = CURDATE()"
+            self.cursor.execute(query)
+            result = self.cursor.fetchall()
+            return result
+        except Error as e:
+            print(f"Failed to select record from MySQL table {e}")
+
+    def withdraw(self, account_no, amount):
+        """Check first if the amount to be withdrawn is within the limit and frequency"""
+        
+        # Get the transaction history of the account within the day and sum up the withdrawals
+        transactions = self.view_day_transactions(account_no)
+        total_withdrawals_amount = 0
+        total_withdrawals_freq = 0
+        for transaction in transactions:
+            if transaction[3] == "Withdraw":
+                total_withdrawals_amount += transaction[4]
+                total_withdrawals_freq += 1
+
+        # Check if the amount to be withdrawn is within the limit and frequency
+        if total_withdrawals_freq > self.view_account(account_no)[5]:
+            return "Exceeded withdrawal frequency for today."
+        elif total_withdrawals_amount > self.view_account(account_no)[4]:
+            return "Exceeded withdrawal limit for today."
+        elif total_withdrawals_amount + amount > self.view_account(account_no)[4]:
+            return "Amount will exceed withdrawal limit for today."
+        else:
+            try:
+                query = f"UPDATE accounts SET balance = balance - {amount} WHERE account_no = {account_no}"
+                self.cursor.execute(query)
+                self.db.commit()
+                return "Done"
+            except Error as e:
+                print(f"Failed to update record in MySQL table {e}")
+    
+    def deposit(self, account_no, amount):
+        """Deposit money into an account"""
+        try:
+            query = f"UPDATE accounts SET balance = balance + {amount} WHERE account_no = {account_no}"
+            self.cursor.execute(query)
+            self.db.commit()
+        except Error as e:
+            print(f"Failed to update record in MySQL table {e}")       
---
+++ ATM_Database.sql
@@ -0,0 +1,30 @@
+-- ATM DATABASE
+CREATE DATABASE atm_system;
+
+USE atm_system;
+
+CREATE TABLE users(
+	userID VARCHAR(10) PRIMARY KEY,
+    surname VARCHAR(255) NOT NULL,
+    fstname VARCHAR(255) NOT NULL,
+    homeadd TEXT NOT NULL,
+    phonenum VARCHAR(11) NOT NULL
+);
+
+CREATE TABLE accounts(
+	account_no INT AUTO_INCREMENT PRIMARY KEY,
+    acctype VARCHAR(255) NOT NULL,
+    userID VARCHAR(10) NOT NULL,
+    balance INT NOT NULL,
+    withdrawal_limit INT NOT NULL,
+    withdrawal_freq INT NOT NULL,
+    FOREIGN KEY (userID) REFERENCES users(userID)
+);
+
+CREATE TABLE transactions(
+	trans_type VARCHAR(10) NOT NULL,
+    amount INT NOT NULL,
+    date_time DATETIME NOT NULL,
+    account_no INT,
+    FOREIGN KEY (account_no) REFERENCES accounts(account_no)
+);
